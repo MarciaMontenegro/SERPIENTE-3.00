d39ec7bc180dcc342b61ddff39b212338d47eaf4
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:05:54 -0500

    Update main.cpp
    my.lib x gusto, implemente las funciones de ahi en metodo de mapa.

--- main.cpp
+++ main.cpp
@@ -2,34 +2,30 @@
 #include <Windows.h>
 #include "mapa.h"
 #include <conio.h>
+//#include "MyLib.h"
 
+bool juego = true;
 
 int main() {
-	mapa mapaP(10, 10, jugador (5,5));
-	while (true){
-		mapaP.gus.demo();
-		if (_kbhit()){
-			switch (_getch()){
-				case 'w':
-					mapaP.gus.movy();
-					break;
-				case 'a':
-					mapaP.gus.mov_x();
-					break;
-				case 's':
-					mapaP.gus.mov_y();
-					break;
-				case 'd':
-					mapaP.gus.movx();
-					break;
-				default:
-					break;
-			}
-		}
-		/*system("CLS");
-		mapaP.imprimir();*/
-		Sleep(10);
+	srand(time(0));
+	int uwu, owo;
+	std::cout << "Dimension en x de su tablero: ";std::cin >> uwu;
+	std::cout << "Dimension en y de su tablero: ";std::cin >> owo;
+	std::cout << "\n";
+	mapa mapaP(uwu, owo, jugador(5, 5,0,0), frutita(3, 3));
+	while (juego) {
+		//std::cout << "\033[H\033[2J\033[3J";
+		system("CLS");
+		mapaP.colision_muros(mapaP.gus.xj, mapaP.x, mapaP.gus.yj, mapaP.y, mapaP.gus.vidas, juego);
+		mapaP.colision_frutita(mapaP.gus.xj, mapaP.manzanita.xf, mapaP.gus.yj, mapaP.manzanita.yf, uwu, owo, mapaP.gus.puntos);
+		mapaP.gus.detectar();
+		mapaP.gus.mover();
+		mapaP.imprimir();
+		mapaP.gus.puntuacion();
+		mapaP.gus.vidainfo();
+		mapaP.gus.mov_cola();
+		Sleep(100);
 	}
-		
+
 	return 0;
 }
