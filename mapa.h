480c07897c4914d40887b69f16e48d2236d7a436
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:06:48 -0500

    Update mapa.h

--- mapa.h
+++ mapa.h
@@ -2,15 +2,22 @@
 #define MAPA_H
 #include<iostream>
 #include "jugador.h"
+#include "frutita.h"
 
-class mapa{
-	public:
-		int x, y;
-		jugador gus;
-		mapa(int x, int y, jugador gus);
-		
-		~mapa();
-		void imprimir();
+class mapa {
+public:
+	int x, y;
+	jugador gus;
+	frutita manzanita;
+	mapa(int x, int y, jugador gus, frutita manzanita);
+
+	~mapa();
+	void imprimir();
+
+	void colision_muros(int& xjj, int& ancho, int& yjj, int& alto, int& vidass, bool& juegoo);
+
+	void colision_frutita(int& xjj, int& mnzx, int& yjj, int& mnzy, int& uwuu, int& owoo, int& puntoss);
+
 };
 
 #endif
