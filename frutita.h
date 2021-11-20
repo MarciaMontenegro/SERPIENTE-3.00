a77612f8b21f54f9a26c86590cc9d1d011d6af8f
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:03:54 -0500

    Update frutita.h

--- frutita.h
+++ frutita.h
@@ -1,1 +1,16 @@
-holi
+#ifndef FRUTITA_H
+#define FRUTITA_H
+
+#include "objeto.h"
+#include <stdlib.h>
+#include <time.h>
+
+class frutita : public objeto {
+public:
+	int xf, yf;
+	frutita();
+	frutita(int xf, int yf);
+	~frutita();
+};
+
+#endif
