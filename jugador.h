1eea528468677e4f99e076ce5a5db9eedd32b7c6
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:05:00 -0500

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -3,18 +3,23 @@
 #include <iostream>
 #include "objeto.h"
 
-class jugador : public objeto{
-	public:
-		int xj;
-		int yj;
-		jugador();
-		jugador(int xj, int yj);
-		~jugador();
-		void movx();
-		void mov_x();
-		void movy();
-		void mov_y();
-		void demo();
+class jugador : public objeto {
+public:
+	int xj, yj, vidas, puntos, colax, colay;
+
+	enum Direcciones { PARADA = 0, IZQ, DRCH, ARRIBA, ABAJO };
+	Direcciones DireccionSerpiente;
+
+	jugador();
+	jugador(int xj, int yj, int colax, int colay);
+	~jugador();
+
+	void detectar();
+	void mover();
+
+	void puntuacion();
+	void vidainfo();
+	void mov_cola();
 };
 
 #endif
