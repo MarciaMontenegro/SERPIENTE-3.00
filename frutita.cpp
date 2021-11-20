fa834830aa00ea199e1d5964ac5e8e056d7d53de
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:03:29 -0500

    Update frutita.cpp

--- frutita.cpp
+++ frutita.cpp
@@ -1,1 +1,15 @@
-holi
+#include "frutita.h"
+#include <stdlib.h>
+#include <time.h>
+
+frutita::frutita() {
+}
+
+frutita::frutita(int _xf, int _yf) {
+	xf = _xf;
+	yf = _yf;
+}
+
+frutita::~frutita() {
+}
+
