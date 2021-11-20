bc6bb74fc29abad8417e84bf98b5d6944ecd71ef
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-20 01:06:25 -0500

    Update mapa.cpp
    nuevos metodos

--- mapa.cpp
+++ mapa.cpp
@@ -1,34 +1,77 @@
 #include "mapa.h"
-
-mapa::mapa(int _x, int _y, jugador _gus){
-	x=_x;
-	y=_y;
-	gus=_gus;
+#include<iostream>
+mapa::mapa(int _x, int _y, jugador _gus, frutita _manzanita) {
+	x = _x;
+	y = _y;
+	gus = _gus;
+	manzanita = _manzanita;
 }
 
-mapa::~mapa()
-{
+mapa::~mapa() {
+
 }
 
-void mapa::imprimir(){
-	/*for(int j=0;j<x+2;j++){
-		std::cout<<"#";
+void mapa::imprimir() {
+	int** matriz_dinamica;
+	int fila, col;
+	fila = y; col = x;
+	matriz_dinamica = new int* [fila];
+	for (int i = 0; i < fila; i++) {
+		matriz_dinamica[i] = new int[col];
 	}
-	std::cout<<"\n";
-	for(int i=0;i<y;i++){
-		std::cout<<"#";
-		for(int j=0;j<x;j++){
-			if(i==b && j==a){
-				std::cout<<"@";
+	for (int i = 0; i < col + 2; i++) {
+		std::cout << "#";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < fila; i++) {
+		std::cout << "#";
+		for (int j = 0; j < col; j++) {
+			if (i == gus.yj && j == gus.xj) {
+				std::cout << "@";
 			}
-			else{
-				std::cout<<" ";
+			else if (i == manzanita.yf && j == manzanita.xf) {
+				std::cout << "*";
+			}
+			else {
+
+				bool MuestraCola = false; //para mostrar la cola
+				for (int k = 0; k < gus.puntos; k++)
+				{
+					if (gus.colax == j && gus.colay == i)
+					{
+						std::cout << "o";
+						MuestraCola = true;
+					}
+				}
+				if (!MuestraCola)
+				{
+					std::cout << " "; //espacio en blanco
+				}
+
+				//std::cout << " ";
 			}
 		}
-		std::cout<<"#\n";
+		std::cout << "#\n";
 	}
-	for(int j=0;j<x+2;j++){
-		std::cout<<"#";
-	}*/
+	for (int i = 0; i < col + 2; i++) {
+		std::cout << "#";
+	}
 }
 
+void mapa::colision_muros(int& xjj, int& ancho, int& yjj, int& alto, int& vidass, bool& juegoo) {
+	if (xjj >= ancho || xjj < 0 || yjj >= alto || yjj < 0) {
+		xjj = (ancho) / 2;
+		yjj = (alto) / 2;
+		vidass--;
+		if (vidass <= 0) { juegoo = false; }
+	}
+}
+
+void mapa::colision_frutita(int& xjj, int& mnzx, int& yjj, int& mnzy, int& uwuu, int& owoo, int& puntoss) {
+	if (xjj == mnzx && yjj == mnzy) {
+		mnzx = rand() % uwuu;
+		mnzy = rand() % owoo;
+		puntoss++;
+	}
+
+}
