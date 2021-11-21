671e9a09c197ffea665232f310437d059705f92e
MarciaMontenegro <91232497+MarciaMontenegro@users.noreply.github.com>
2021-11-21 13:55:48 -0500

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -5,21 +5,27 @@
 
 class jugador : public objeto {
 public:
-	int xj, yj, vidas, puntos, colax, colay;
+	int xj, yj, vidas, puntos;
+	int colax[100], colay[100];
 
 	enum Direcciones { PARADA = 0, IZQ, DRCH, ARRIBA, ABAJO };
 	Direcciones DireccionSerpiente;
+	enum Direcciones2 { PARADA2 = 0, IZQ2, DRCH2, ARRIBA2, ABAJO2, SALIR };
+	Direcciones2 direccionserpiente2;
 
 	jugador();
-	jugador(int xj, int yj, int colax, int colay);
+	jugador(int xj, int yj);
 	~jugador();
-
 	void detectar();
 	void mover();
-
 	void puntuacion();
 	void vidainfo();
 	void mov_cola();
+	void colision_cola();
+
+
+
+
 };
 
 #endif
